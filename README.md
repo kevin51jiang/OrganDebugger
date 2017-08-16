8190031f54142cae6261573db62e5b8b67e4d290
mob41 <puihuenlaw@gmail.com>
2017-08-16 17:10:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# OrganDebugger
+A simple exception location debugger API
+
+This repository is for better tidying of osumer source dependencies.
---
+++ src/main/java/com/github/mob41/organdebug/exceptions/OrganException.java
@@ -0,0 +1,29 @@
+package com.github.mob41.organdebug.exceptions;
+
+public class OrganException extends Exception {
+
+    public OrganException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public OrganException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public OrganException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public OrganException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public OrganException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+}
